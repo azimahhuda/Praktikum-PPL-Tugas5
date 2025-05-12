d7bc78a41de2a0eeae942b8d1fb280701c09ee7c
azimahhuda <azimahhuda@gmail.com>
2025-05-12 10:50:08 +0700

    setup awal project

---
+++ adonis-api-song/app/controllers/lagus_controller.ts
@@ -0,0 +1,9 @@
+// import type { HttpContext } from '@adonisjs/core/http'
+
+export default class LaguController {
+  // Get all lagu
+//   async index({ response }: HttpContext) {
+//     const lagu = await Lagu.all()
+//     return response.json({ data: lagu })
+//   }
+}
