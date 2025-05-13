bf76e7f66eeda45de701acfbf496dfff02e15360
Cut Dahliana <cutdahli@mhs.usk.ac.id>
2025-05-13 16:51:26 +0700

    add method in controller and set routes

--- adonis-api-song/app/controllers/lagus_controller.ts
+++ adonis-api-song/app/controllers/lagus_controller.ts
@@ -1,9 +1,49 @@
-// import type { HttpContext } from '@adonisjs/core/http'
+import type { HttpContext } from '@adonisjs/core/http'
+import Lagu from '#models/lagu'
+
 
 export default class LaguController {
   // Get all lagu
-//   async index({ response }: HttpContext) {
-//     const lagu = await Lagu.all()
-//     return response.json({ data: lagu })
-//   }
+  async index({ response }: HttpContext) {
+    const lagu = await Lagu.all()
+    return response.json({ data: lagu })
+  }
+
+  // Get 1 lagu by id
+  async show({ params, response }: HttpContext) {
+    const lagu = await Lagu.find(params.id)
+    if (!lagu) {
+      return response.status(404).json({ message: 'Lagu tidak ditemukan' })
+    }
+    return response.json(lagu)
+  }
+
+  // Create lagu
+  async store({ request, response }: HttpContext) {
+    const data = request.only(['penyanyi', 'judul_lagu', 'genre', 'nama_album'])
+    const lagu = await Lagu.create(data)
+    return response.status(201).json(lagu)
+  }
+
+  // Update lagu
+  async update({ params, request, response }: HttpContext) {
+    const lagu = await Lagu.find(params.id)
+    if (!lagu) {
+      return response.status(404).json({ message: 'Lagu tidak ditemukan' })
+    }
+    const data = request.only(['penyanyi', 'judul_lagu', 'genre', 'nama_album'])
+    lagu.merge(data)
+    await lagu.save()
+    return response.json(lagu)
+  }
+
+  // Delete lagu
+  async destroy({ params, response }: HttpContext) {
+    const lagu = await Lagu.find(params.id)
+    if (!lagu) {
+      return response.status(404).json({ message: 'Lagu tidak ditemukan' })
+    }
+    await lagu.delete()
+    return response.json({ message: 'Lagu berhasil dihapus' })
+  }
 }
--- adonis-api-song/start/routes.ts
+++ adonis-api-song/start/routes.ts
@@ -8,5 +8,14 @@
 */
 
 import router from '@adonisjs/core/services/router'
+import LaguController from '#controllers/lagus_controller'
 
-router.get('/', async () => 'It works!')
+router.get('/', async ({ response }) => {
+  return response.redirect('/lagu')
+})
+
+router.get('/lagu', [LaguController, 'index'])
+router.get('/lagu/:id', [LaguController, 'show'])
+router.post('/lagu', [LaguController, 'store'])
+router.put('/lagu/:id', [LaguController, 'update'])
+router.delete('/lagu/:id', [LaguController, 'destroy'])
